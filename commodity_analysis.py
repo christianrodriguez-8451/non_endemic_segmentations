bfb94285915752dc02f6835f4ecd5fb132c4a66d
christianrodriguez-8451 <christianrodriguez-8451>
2023-07-11 22:34:52 +0000

    Consolidated the workflow into one program. commodity_analysis.py now executes for both the commodity and sub_commodity level.

--- commodity_analysis.py
+++ commodity_analysis.py
@@ -1,4 +1,27 @@
 # Databricks notebook source
+"""Pulls all transactions for the given year and gets the complete
+household set at the commodity and sub-commodity level. With the household sets,
+we proceed to calculate household counts for every commodity and sub-commodity.
+At both the commodity and sub-commodity level, we extract the top words and
+a sample of the products to have descriptive information on the given
+commodity or sub-commodity. The final output are two pipe delimited files that
+contain household counts, top words, and product sample at the commodity and
+sub-commodity level.
+
+To Do:
+  Write out the final output as a single excel workbook instead of two
+  pipe delimtiedn files. Doing so will streamline the process.
+
+  For the product samples column, consider using PIM instead of PID to
+  get around the 20 character limit for product names.
+
+  In the top words column, account for stop words! Think words that are
+  most common in the complete product level (for the commodity level) and
+  in the commodity level (for the sub-commodity level) as well.
+"""
+
+# COMMAND ----------
+
 #Define service principals
 service_credential = dbutils.secrets.get(scope='kv-8451-tm-media-dev',key='spTmMediaDev-pw')
 service_application_id = dbutils.secrets.get(scope='kv-8451-tm-media-dev',key='spTmMediaDev-app-id')
@@ -16,67 +39,38 @@ spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.c
 
 # COMMAND ----------
 
-def get_latest_modified_directory(pDirectory):
-    """
-    get_latest_modified_file_from_directory:
-        For a given path to a directory in the data lake, return the directory that was last modified. 
-        Input path format expectation: 'abfss://x@sa8451x.dfs.core.windows.net/
-    """
-    #Set to get a list of all folders in this directory and the last modified date time of each
-    vDirectoryContentsList = list(dbutils.fs.ls(pDirectory))
-
-    #Convert the list returned from get_dir_content into a dataframe so we can manipulate the data easily. Provide it with column headings. 
-    #You can alternatively sort the list by LastModifiedDateTime and get the top record as well. 
-    df = spark.createDataFrame(vDirectoryContentsList,['FullFilePath', 'LastModifiedDateTime'])
-
-    #Get the latest modified date time scalar value
-    maxLatestModifiedDateTime = df.agg({"LastModifiedDateTime": "max"}).collect()[0][0]
-
-    #Filter the data frame to the record with the latest modified date time value retrieved
-    df_filtered = df.filter(df.LastModifiedDateTime == maxLatestModifiedDateTime)
-    
-    #return the file name that was last modifed in the given directory
-    return df_filtered.first()['FullFilePath']
-
-#pim_path = "abfss://pim@sa8451posprd.dfs.core.windows.net/pim_core/by_cycle/"
-pim_path = "abfss://pim@sa8451posprd.dfs.core.windows.net/pim_core/by_cycle/cycle_date=20230520/"
-pim_core = spark.read.parquet(pim_path)
-pim_core = pim_core.select("upc", "gtin", "familyTree.commodity.name", "familyTree.subcommodity.name", "gtinName")
-pim_core = pim_core.withColumnRenamed("familyTree.commodity.name", "familyTree.commodity.commodity")
-#pim_core.columns = ["upc", "gtin", "commodity", "sub_commodity", "product_name"]
-#Standardize the product name - eliminate trailing white space and capitalize it
-pim_core.show(1, truncate=False)
-
-# COMMAND ----------
-
-pim_path = "abfss://pim@sa8451posprd.dfs.core.windows.net/pim_core/by_cycle/"
-dbutils.fs.ls(pim_path)
-
-# COMMAND ----------
-
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from effodata import ACDS, golden_rules, Joiner, Sifter, Equality, sifter, join_on, joiner
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from pyspark.sql.functions import countDistinct, lit, sum, max
 
+year = "2022"
 #Extract the relevant PID cycles for 2022 (year of analysis)
-#stub_path = "abfss://acds@sa8451posprd.dfs.core.windows.net/product/by_cycle/product_dim_{}/"
 stubs_path = "abfss://acds@sa8451posprd.dfs.core.windows.net/product/by_cycle/"
 cycles = dbutils.fs.ls(stubs_path)
 cycles = list(cycles)
 cycles = [c[0] for c in cycles]
 cycles = [c.split("dim_")[1] for c in cycles]
 cycles = [c.strip("/") for c in cycles]
-cycles = [c for c in cycles if c[0:4] == "2022"]
+cycles = [c for c in cycles if c[0:4] == year]
 
 #Empty container to catch dataframes as we iterate by cycle
-schema = StructType([
+#Todo: Create an additional schema and empty container for sub-commodities
+schema_c = StructType([
   StructField('commodity_desc', StringType(), True),
   StructField('ehhn', IntegerType(), True),
   StructField('cycle', StringType(), True),
   ])
-subcomm_hh = spark.createDataFrame([], schema)
+comm_hh = spark.createDataFrame([], schema_c)
+
+schema_s = StructType([
+  StructField('commodity_desc', StringType(), True),
+  StructField('sub_commodity_desc', StringType(), True),
+  StructField('ehhn', IntegerType(), True),
+  StructField('cycle', StringType(), True),
+  ])
+subcomm_hh = spark.createDataFrame([], schema_s)
 for i in range(0, len(cycles)):
   if i != (len(cycles) - 1):
     prior = cycles[i]
@@ -89,13 +83,15 @@ for i in range(0, len(cycles)):
     now = cycles[i]
     now = datetime.fromisoformat("{}-{}-{}".format(now[0:4], now[4:6], now[6:8]))
 
-  #Message to keep track what cycle we are on
+  #Print message to keep track what cycle we are on
   message = "{}th cycle is from {} to {}!".format(i, prior, now)
   print(message)
 
   #Read in the correct PID cycle for the given time frame of the year
+  #Todo: Keep sub-commodities as well
+  stub_path = "abfss://acds@sa8451posprd.dfs.core.windows.net/product/by_cycle/product_dim_{}/"
   pid = spark.read.parquet(stub_path.format(prior))
-  keep_cols = ["con_upc_no", "commodity_desc"]
+  keep_cols = ["con_upc_no", "commodity_desc", "sub_commodity_desc"]
   pid = pid.select(keep_cols)
   del(message, keep_cols)
 
@@ -109,96 +105,102 @@ for i in range(0, len(cycles)):
   #Bump PID + ACDS to get unique households per sub-commodity and then add to our container
   pid = pid.withColumnRenamed("con_upc_no", "gtin_no")
   hh = acds.join(pid, on="gtin_no", how="inner")
+  hh.cache()
+  
   dup_cols = ["commodity_desc", "ehhn"]
-  hh = hh.dropDuplicates(dup_cols)
-  hh = hh.select(dup_cols)
-  hh = hh.withColumn("cycle", lit(prior))
-  subcomm_hh = subcomm_hh.union(hh)
+  temp = hh.dropDuplicates(dup_cols)
+  temp = temp.select(dup_cols)
+  temp = temp.withColumn("cycle", lit(prior))
+  comm_hh = comm_hh.union(temp)
+
+  dup_cols = ["commodity_desc", "sub_commodity_desc", "ehhn"]
+  temp = hh.dropDuplicates(dup_cols)
+  temp = temp.select(dup_cols)
+  temp = temp.withColumn("cycle", lit(prior))
+  subcomm_hh = subcomm_hh.union(temp)
+  
   del(pid, acds, hh, dup_cols, prior)
 
-subcomm_hh.cache()
-subcomm_hh.show(25)
-
-# COMMAND ----------
-
-#In the past year, get the unique household count per sub-commodity
-hh_counts = subcomm_hh.\
+#In the past year, get the unique household count per commodity/sub-commodity
+comm_hh_counts = comm_hh.\
   groupBy(["commodity_desc"]).\
   agg(countDistinct("ehhn").alias("households_count"))
-hh_counts.cache()
-hh_counts.is_cached
+comm_hh_counts = comm_hh_counts.withColumnRenamed("commodity_desc", "commodity")
+comm_hh_counts.cache()
+
+subcomm_hh_counts = subcomm_hh.\
+  groupBy(["commodity_desc", "sub_commodity_desc"]).\
+  agg(countDistinct("ehhn").alias("households_count"))
+subcomm_hh_counts = subcomm_hh_counts.withColumnRenamed("commodity_desc", "commodity")
+subcomm_hh_counts = subcomm_hh_counts.withColumnRenamed("sub_commodity_desc", "sub_commodity")
+subcomm_hh_counts.cache()
+
+comm_hh_counts.show(10)
+subcomm_hh_counts.show(10)
 
 # COMMAND ----------
 
-#~5900 sub-commodities
-hh_counts.count()
+#Get a quick and dirty count
+#2022 - 834 commodities
+#2022 - 5407 sub-commodities
+print(comm_hh_counts.count())
+print(subcomm_hh_counts.count())
 
 # COMMAND ----------
 
-#Write out your household counts
-output_fp = "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/Users/c127414/commodity_hh_count.csv"
-hh_counts.write.csv(output_fp)
-
-# COMMAND ----------
-
+import config as con
 from pyspark.sql.window import Window
-from pyspark.sql.functions import col, row_number, lit, collect_list, split, substring, explode
+from pyspark.sql.functions import col, row_number, lit, collect_list, split, substring, explode, concat_ws
 
+#Read in PID and only keep the necessary columns
 stub_path = "abfss://acds@sa8451posprd.dfs.core.windows.net/product/current/"
 pid = spark.read.parquet(stub_path)
-pid = pid.select("commodity_desc", "con_dsc_tx")
-
-#Get the complete word occurence in each sub-commodity
-word_count = pid.select("commodity_desc", explode(split(pid["con_dsc_tx"], " ")).alias("word"))
-word_count = word_count.groupBy(["commodity_desc", "word"]).count()
-word_count = word_count.sort(["commodity_desc", "count"], ascending=False)
-
-#Get the top n words in each sub-commodity
-subcomm_windows = Window.partitionBy("commodity_desc").orderBy(col("count").desc())
-n = 100
-top_n = word_count.withColumn("row", row_number().over(subcomm_windows)).filter(col("row") <= n)
-top_n = top_n.groupBy('commodity_desc').agg(collect_list('word').alias('top_words'))
-
-#Get a sample of s products for each sub-commodity
+pid = pid.select("commodity_desc", "sub_commodity_desc", "con_dsc_tx")
+pid = pid.withColumnRenamed("commodity_desc", "commodity")
+pid = pid.withColumnRenamed("sub_commodity_desc", "sub_commodity")
 pid = pid.withColumn("dummy", lit(1))
-product_windows = Window.partitionBy("commodity_desc").orderBy(col("dummy"))
+#How many top words to pull from the product names
+n = 100
+#How many product samples to pull per commodity/sub-commodity
 s = 20
-product_samples = pid.withColumn("row", row_number().over(product_windows)).filter(col("row") <= s)
-product_samples = product_samples.groupBy('commodity_desc').agg(collect_list('con_dsc_tx').alias('product_samples'))
-product_samples.show(50)
 
-# COMMAND ----------
+#For both commodities and sub-commodities
+for comm in [["commodity", comm_hh_counts], ["sub_commodity", subcomm_hh_counts]]:
+  column = comm[0]
 
-hh_path = "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/Users/c127414/commodity_hh_count.csv"
-hh_counts = spark.read.csv(hh_path, header=False)
-hh_counts = hh_counts.toDF("commodity_desc", "hh_counts")
+  #Get the complete word occurence in each commodity/sub-commodity
+  word_count = pid.select(column, explode(split(pid["con_dsc_tx"], " ")).alias("word"))
+  word_count = word_count.groupBy([column, "word"]).count()
+  word_count = word_count.sort([column, "count"], ascending=False)
+  #Get the top n words in each commodity/sub-commodity
+  comm_windows = Window.partitionBy(column).orderBy(col("count").desc())
+  top_n = word_count.withColumn("row", row_number().over(comm_windows)).filter(col("row") <= n)
+  top_n = top_n.groupBy(column).agg(collect_list('word').alias('top_words'))
+  #Turn list-columns into string columns
+  top_n = top_n.withColumn("top_words", concat_ws(", ", "top_words"))
+  top_n = top_n.select(column, "top_words")
 
-#Merge top words and the product sample against the sub-commodity household counts
-desc_data = top_n.join(product_samples, on="commodity_desc", how="inner")
-hh_counts = hh_counts.join(desc_data, on="commodity_desc", how="left")
-hh_counts = hh_counts.withColumnRenamed("commodity_desc", "commodity")
-hh_counts = hh_counts.withColumnRenamed("hh_counts", "household_counts")
-hh_counts = hh_counts.select("commodity", "household_counts", "top_words", "product_samples")
-hh_counts = hh_counts.sort(["commodity"])
+  #Get a sample of s products for each commodity/sub-commodity
+  product_windows = Window.partitionBy(column).orderBy(col("dummy"))
+  product_samples = pid.withColumn("row", row_number().over(product_windows)).filter(col("row") <= s)
+  product_samples = product_samples.groupBy(column).agg(collect_list('con_dsc_tx').alias('product_samples'))
+  #Turn list-columns into string columns
+  product_samples = product_samples.withColumn("product_samples", concat_ws(", ", "product_samples"))
+  product_samples = product_samples.select(column, "product_samples")
 
-#Need to turn list-columns into string columns
-from pyspark.sql.functions import concat_ws
-hh_counts = hh_counts.withColumn("top_words", concat_ws(", ", "top_words"))
-hh_counts = hh_counts.withColumn("product_samples", concat_ws(", ", "product_samples"))
-hh_counts.show(50)
+  #Merge top words and the product sample against the commodity/sub-commodity household counts
+  desc_data = top_n.join(product_samples, on=column, how="inner")
+  hh_counts = comm[1]
+  hh_counts = hh_counts.join(desc_data, on=column, how="inner")
 
-# COMMAND ----------
+  #Write out as a consolidate pipe delimited file
+  save_location= con.hh_counts_fp
+  csv_location = save_location+"temp.folder"
+  file_location = save_location + column + '_data_' + year + '.pipe'
 
-save_location= "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/Users/c127414/"
-csv_location = save_location+"temp.folder"
-file_location = save_location+'commodity_data.pipe'
+  hh_counts.repartition(1).write.csv(path=csv_location, mode="overwrite", header=True, sep="|")
 
-hh_counts.repartition(1).write.csv(path=csv_location, mode="overwrite", header=True, sep="|")
-
-file = dbutils.fs.ls(csv_location)[-1].path
-dbutils.fs.cp(file, file_location)
-dbutils.fs.rm(csv_location, recurse=True)
-
-# COMMAND ----------
-
+  my_file = dbutils.fs.ls(csv_location)[-1].path
+  dbutils.fs.cp(my_file, file_location)
+  dbutils.fs.rm(csv_location, recurse=True)
 
