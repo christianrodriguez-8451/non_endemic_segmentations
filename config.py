9f3ac9a7e884a9b2b12cd055e3bfc3938bc92250
christianrodriguez-8451 <christianrodriguez-8451>
2023-07-11 22:36:57 +0000

    Added a configuration file to consolidate critical variables. Makes it easy for other process to pull these variables.

---
+++ config.py
@@ -0,0 +1,8 @@
+#Location of segmentations control file live in production
+control_fp = "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/commodity_segments/commodity_segments_control.csv"
+
+#Where the commodity-segment's output lands
+output_fp = "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/commodity_segments/output/"
+
+#Where the exploratory household analysis lands
+hh_counts_fp = "abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/commodity_segments/household_counts/"
