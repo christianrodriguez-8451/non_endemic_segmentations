aa3a52ea0a9830d37eaee359543747302857f26a
stevenmartz-8451 <stevenmartz-8451>
2023-07-12 15:54:03 +0000

    Fixed for in logic to run through list

--- Search_Embedding.py
+++ Search_Embedding.py
@@ -80,7 +80,7 @@ diet_query_embeddings_directories_list = diet_query_embeddings_directories.rdd.m
 
 dbutils.widgets.text("embedding_query", "")
 embedding_query = dbutils.widgets.get("embedding_query")
-embedding_query=embedding_query.split(",")
+embedding_query= embedding_query.split(",")
 
 #When we move forward with more of those below, we will need two different lists.  One for the query and one to create 
 # 'FREE FROM GLUTEN', 'KETOGENIC', 'Lactose free', 'MACROBIOTIC', 'PALEO', 'VEGAN', 'VEGETARIAN', 'COELIAC', 'AYERVEDIC', 'LOW BACTERIA', 'DIABETIC', 
@@ -128,8 +128,7 @@ def create_array_query(query_vector):
 # COMMAND ----------
 
 if embedding_query:
-  embedding_queries = [embedding_query]
-  for query in embedding_queries:
+  for query in embedding_query:
     query = ''.join(query).lower()
     # Encoding the query, make it a pyspark vector we can take the dot product of later
     query_vector = model.encode(query, normalize_embeddings = True).tolist()
