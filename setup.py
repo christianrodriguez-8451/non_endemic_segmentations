9c048580a9c8edc954cfb8ac0d2df13cb9f504f0
christianrodriguez-8451 <christianrodriguez-8451>
2023-07-13 01:23:03 +0000

    Eliminated entries carried over from dummy example.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-#Stolen from Patrick F.'s example
+#Inspired from Patrick F.'s ML Ops example
 from setuptools import setup, find_packages
 
-setup(name='non_endemic_structure',
+setup(name='non_endemic_segmentations',
       version='1.0.0',
       description='Package to create and maintain the Non-Endemic Segmentations supporting PRISM.',
       #Add Steven Martz?
       author='Christian Rodriguez',
       #Add Steven Martz?
       author_email='christian.rodriguez@8451.com',
-      #url='https://github.com/8451LLC/mlops_media_science',
+      url='https://github.com/christianrodriguez-8451/non_endemic_segmentations.git',
       #packages=find_packages(),
       packages=["non_endemic_segmentations"],
       install_requires=[
