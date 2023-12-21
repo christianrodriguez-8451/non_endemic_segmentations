7a69e4e522bc5f12a4dbb23d5b998c29fcbe8852
christian.rodriguez@8451.com <christian.rodriguez@8451.com>
2023-12-21 19:13:17 +0000

    Notebook to demonstrate how to use classes in config.py

---
+++ toolbox/config_example_usage.py
@@ -0,0 +1,39 @@
+# Databricks notebook source
+import toolbox.config as con
+
+# COMMAND ----------
+
+con.segmentations.fuel_segmentations
+
+# COMMAND ----------
+
+con.segmentations.geospatial_segmentations
+
+# COMMAND ----------
+
+con.segmentations.funlo_segmentations
+
+# COMMAND ----------
+
+con.segmentations.all_segmentations
+
+# COMMAND ----------
+
+segment = con.segmentation("macrobiotic")
+segment.name
+
+# COMMAND ----------
+
+segment.directory
+
+# COMMAND ----------
+
+segment.files
+
+# COMMAND ----------
+
+segment.type
+
+# COMMAND ----------
+
+segment.propensities
