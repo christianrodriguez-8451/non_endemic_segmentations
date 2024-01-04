1836335846b942e7654b25e43dc43ee9f6a5657a
j468635@8451.com <j468635@8451.com>
2024-01-04 17:04:18 +0000

    pushing content dashboard script

--- monitoring/content_dashboard_script.py
+++ monitoring/content_dashboard_script.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md 
+# MAGIC #Set up
+
+# COMMAND ----------
+
 # common python packages
 import pandas as pd
 import numpy as np
@@ -24,13 +29,60 @@ import re
 
 # COMMAND ----------
 
-# eventually read in config file
+# https://github.com/christianrodriguez-8451/non_endemic_segmentations/blob/main/toolbox/config.py
 
 import toolbox.config as config
 
 # COMMAND ----------
 
+# MAGIC %md 
+# MAGIC # function
+
+# COMMAND ----------
+
+def extract_numeric_value(input_string):
+  """
+  Extracts and returns the numeric value following an underscore '_' in the input string.
+
+  Parameters:
+  - input_string (str): The input string from which to extract the numeric value.
+
+  Returns:
+  - str or None: If a numeric value following an underscore is found, it is returned as a string.
+                  If no such value is found, returns None.
+  """ 
+  
+  match = re.search(r'_(\d+)', input_string)
+  if match:
+    return match.group(1)
+  else:
+    return None
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # funlo segmentations
+
+# COMMAND ----------
+
+# taking a look at what the funlo files look like 
+reading = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/embedded_dimensions/customer_data_assets/segment_behavior/segmentation/modality=free_from_gluten/stratum_week=20230701")
+reading.display()
+
+# COMMAND ----------
+
 def create_df_groupby_propensity_time(segmentation):
+  """
+  Create a grouped DataFrame by stratum_week with aggregated household counts
+  for a specific segment and its associated propensities over time.
+
+  Parameters:
+  - segmentation (str): The segment identifier for which the DataFrame is created.
+
+  Returns:
+  - DataFrame: A DataFrame grouped by date with columns indicating the time,
+                segment, and propensity, along with the aggregated household counts.
+  """
 
   # getting the file path
   root = config.get_directory(segmentation)
@@ -73,55 +125,53 @@ def create_df_groupby_propensity_time(segmentation):
 # COMMAND ----------
 
 # initiating an empty df for the loop
-all_segment_master_file = None
+funlo_segment_master_file = None
 
 for i in config.segmentations.funlo_segmentations:
 
   run_segment = create_df_groupby_propensity_time(i)
 
-  if all_segment_master_file is None:
-    all_segment_master_file = run_segment
+  if funlo_segment_master_file is None:
+    funlo_segment_master_file = run_segment
   else:
-    all_segment_master_file = all_segment_master_file.union(run_segment)
+    funlo_segment_master_file = funlo_segment_master_file.union(run_segment)
 
 # COMMAND ----------
 
-all_segment_master_file.display()
+funlo_segment_master_file.display()
 
 # COMMAND ----------
 
-readin = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/percentile_segmentations/fitness_enthusiast/fitness_enthusiast_20231115")
+# MAGIC %md 
+# MAGIC # percentile segmentations
 
 # COMMAND ----------
 
-readin.display()
+# taking a look at percentile segmentation delta structure
+
+reading = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/percentile_segmentations/fitness_enthusiast/fitness_enthusiast_20231115")
+reading.display()
 
 # COMMAND ----------
-
-config.get_directory("fitness_enthusiast")
-
-# COMMAND ----------
-
-def extract_numeric_value(input_string):
-    match = re.search(r'_(\d+)', input_string)
-    if match:
-        return match.group(1)
-    else:
-        return None
-
-# COMMAND ----------
-
-config.get_files("fitness_enthusiast")
-
-# COMMAND ----------
-
-print(extract_numeric_value('fitness_enthusiast_20231115/'))
-
-# COMMAND ----------
-
 
 def access_percentile_segs(segmentation):
-  
+  """
+  Access and aggregate data for a specific segment based on percentiles, considering date information.
+
+  Parameters:
+  - segmentation (str): The segment identifier for which the DataFrame is accessed.
+
+  Returns:
+  - DataFrame: A DataFrame grouped by date with columns indicating the time,
+                segment, and propensity, along with the aggregated household counts.
+
+  Dependencies:
+  - The function relies on configuration details obtained through the config module,
+    including directory paths, propensity compositions, and file details.
+  - The function also depends on the availability of the `extract_numeric_value` function
+    for extracting date information from file names.
+  """
+
   # getting the file path
   root = config.get_directory(segmentation)
 
@@ -167,16 +217,6 @@ def access_percentile_segs(segmentation):
 
 # COMMAND ----------
 
-test = access_percentile_segs("beautists")
-test.display()
-
-# COMMAND ----------
-
-readin2 = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/fuel_segmentations/gasoline/gasoline_20231110")
-readin2.display()
-
-# COMMAND ----------
-
 # initiating an empty df for the loop
 percentile_results = None
 
@@ -199,16 +239,28 @@ config.get_directory("casual_auto_fixers")
 
 # COMMAND ----------
 
-spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/percentile_segmentations/casual_auto_fixers/")
+# what does auto fixers look like 
+
+# taking a look at percentile segmentation delta structure
+
+reading = spark.read.format("delta").load('abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/percentile_segmentations/casual_auto_fixers/casual_auto_fixers_20231222")
+reading.display()
 
 # COMMAND ----------
+
+# thankfully casual auto fixers is the last item in the list
 
 config.segmentations.percentile_segmentations 
 
 # COMMAND ----------
 
-readin2 = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/fuel_segmentations/gasoline/gasoline_20231110")
-readin2.display()
+# MAGIC %md
+# MAGIC #fuel segmentations
+
+# COMMAND ----------
+
+reading = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/fuel_segmentations/gasoline/gasoline_20231110")
+reading.display()
 
 # COMMAND ----------
 
@@ -230,8 +282,15 @@ fuel_results.display()
 
 # COMMAND ----------
 
-readin2 = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/fuel_segmentations/gasoline/gasoline_20231110")
-readin2.display()
+# MAGIC %md
+# MAGIC # geospatial segmentations
+
+# COMMAND ----------
+
+# inspecting how geospatial looks 
+
+reading = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/geospatial/roadies/roadies_20231117")
+reading.display()
 
 # COMMAND ----------
 
@@ -253,90 +312,43 @@ geospatial_results.display()
 
 # COMMAND ----------
 
-unioned_df = all_segment_master_file.union(percentile_results).union(fuel_results).union(geospatial_results)
+# MAGIC %md
+# MAGIC # union
 
 # COMMAND ----------
 
-readin2 = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/commodity_segments/fuel_segmentations/gasoline/gasoline_20231110")
-readin2.display()
+unioned_df = funlo_segment_master_file.union(percentile_results).union(fuel_results).union(geospatial_results)
 
 # COMMAND ----------
 
-segment_list = [segment for segment_group in segmentations for segment in segmentations[segment_group]]
+total_seg = unioned_df.select(f.col("segment")).distinct().count()
+print(f"Total segmentations in the dashboard is: {total_seg}")
 
 # COMMAND ----------
 
-print(segment_list)
+unioned_df.display()
 
 # COMMAND ----------
 
-# temp = read_and_groupby("macrobiotic")
-# temp.show()
+
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # loop
+# MAGIC #write to blob
 
 # COMMAND ----------
 
-def read_and_groupby(modality):
-  """
-  arguments: 
-
-  output:
-  """
-  read_in = spark.read.format("delta").load(f"abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/embedded_dimensions/customer_data_assets/segment_behavior/segmentation/modality={modality}")
-
-  # Group by date and count distinct ehhn
-  groupby_df = ((read_in
-                    .groupBy("stratum_week", "segment")
-                    .agg(f.countDistinct("ehhn").alias("hh_count"))
-                    ).withColumnRenamed("stratum_week", "time")
-                    .withColumnRenamed("segment", "modality")
-                    .withColumn("segmentation", f.lit(f"{modality}"))
-  )
-
-  return groupby_df
-  
-
-# COMMAND ----------
-
-# initialize an empty df to hold the appended data
-segmentation_agg_df = None
-
-for i in segment_list:
-
-  current_df = read_and_groupby(i)
-
-  # append the df to the master df
-  if segmentation_agg_df is None:
-    segmentation_agg_df = current_df
-
-  else:
-    segmentation_agg_df = segmentation_agg_df.union(current_df)
-
-# COMMAND ----------
-
-segmentation_agg_df.display()
+unioned_df.write.mode("overwrite").parquet('abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/j468635/content_dashboard/segmentation_agg_df')
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##write to blob
+# MAGIC # time df
 
 # COMMAND ----------
 
-all_segment_master_file.write.mode("overwrite").parquet('abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/j468635/content_dashboard/segmentation_agg_df')
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC # time master
-
-# COMMAND ----------
-
-time_master_0 = all_segment_master_file.select(f.col("time")).distinct()
+time_master_0 = unioned_df.select(f.col("time")).distinct()
 
 time_master_1 = time_master_0.withColumn("time", f.col("time").cast("int"))
 
@@ -351,6 +363,25 @@ time_master_2.display()
 # COMMAND ----------
 
 time_master_2.write.mode("overwrite").parquet('abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/j468635/content_dashboard/time')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #scratch
+
+# COMMAND ----------
+
+lowcount = spark.read.format("delta").load("abfss://media@sa8451dbxadhocprd.dfs.core.windows.net/audience_factory/embedded_dimensions/customer_data_assets/segment_behavior/segmentation/modality=free_from_gluten/stratum_week=20231202")
+
+# COMMAND ----------
+
+# i want to verify the groupby really is seeing less than 100 H ehhn 
+
+lowcount.filter(f.col("segment") == "H").display()
+
+# COMMAND ----------
+
+unioned_df.filter(f.col("hh_count") <= 500).display()
 
 # COMMAND ----------
 
