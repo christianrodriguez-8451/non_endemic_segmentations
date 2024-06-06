e8cec2b1fb6f7aba261d8e6d951c34c726b5959b
christianrodriguez-8451 <christianrodriguez-8451>
2024-06-06 19:30:25 +0000

    Reducing code size and adjusting so that the audiences it produces are controlled by a dictionary.

--- products_embedding/Search_Embedding.py
+++ products_embedding/Search_Embedding.py
@@ -3,75 +3,72 @@
 import resources.config as config
 import src.utils as utils
 import products_embedding.src.date_calcs as dates
+from products_embedding.config import sentences_dict
+
+#Set-up widgets
+config.dbutils.widgets.text("embedding_name", "")
+config.dbutils.widgets.text("embedding_sentence_query", "")
+
 # COMMAND ----------
 
-# Set path for product vectors
+#Read in latest product vectors that will be used in dot product
 product_vectors_path = config.get_latest_modified_directory(config.product_vectors_description_path)
 product_vectors_df = config.spark.read.format("delta").load(product_vectors_path)
 
-# Set path for product vectors
-upc_list_path = config.get_latest_modified_directory(config.embedded_dimensions_dir + config.diet_query_dir)
-diet_query_embeddings_directories = config.spark.createDataFrame(list(config.dbutils.fs.ls(upc_list_path)))
-diet_query_embeddings_directories = \
-    diet_query_embeddings_directories.filter(diet_query_embeddings_directories.name !=
-                                             'embedding_sentence_query_lookup/')
-diet_query_embeddings_directories_list = diet_query_embeddings_directories.rdd.map(lambda column: column.name).collect()
-embedding_sentence_query_lookup = upc_list_path + 'embedding_sentence_query_lookup'
-embedding_sentence_query_lookup_df = config.spark.read.format("delta").load(embedding_sentence_query_lookup)
-
-config.dbutils.widgets.text("embedding_name", "")
-config.dbutils.widgets.text("embedding_sentence_query", "")
+#Get boolean that checks if the notebook is running via widgets
 embedding_name_check = "embedding_name"
 embedding_sentence_query_check = "embedding_sentence_query"
 is_empty = utils.pyspark_databricks_widget_check_for_empty_text_field(embedding_name_check)
 
-# COMMAND ----------
-
-# taking dot product of query vector and product vector
-# order by dot product descending
-
+#If the notebook is operating via widget...
 if not is_empty:
-    embedding_name = config.dbutils.widgets.get("embedding_name")
-    embedding_sentence_query = config.dbutils.widgets.get("embedding_sentence_query")
-    embedding_sentence_query = ''.join(embedding_sentence_query).lower()
-    # Encoding the query, make it a pyspark vector we can take the dot product of later
-    query_vector = utils.model.encode(embedding_sentence_query, normalize_embeddings=True).tolist()
-    search_df = utils.create_search_df(utils.create_dot_df(product_vectors_df,
-                                                           utils.create_array_query(query_vector)))
-    embedding_name = embedding_name.replace(' ', '_')
-    embedding_name = embedding_name.replace('/', '')
-    search_df.write.mode("overwrite").format("delta").save(upc_list_path + embedding_name)
-    print(f'Wrote {embedding_name} upc list here: {upc_list_path}')
-    data = [(embedding_name, embedding_sentence_query)]
-    columns = ["name", "query"]
-    embedding_sentence_query = config.spark.createDataFrame(data, columns)
-    embedding_sentence_query_lookup_df = embedding_sentence_query_lookup_df.union(embedding_sentence_query)
-    # write look up file of upc list location
-    embedding_sentence_query_lookup_df.write.mode("overwrite").format("delta").save(upc_list_path
-                                                                                    +
-                                                                                    '/embedding_sentence_query_lookup')
-    print(f'Wrote {embedding_sentence_query} query here: {upc_list_path}')
+  #Extract the inputted parameters
+  name = config.dbutils.widgets.get("embedding_name")
+  sentence_query = config.dbutils.widgets.get("embedding_sentence_query")
+  sentence_query = ''.join(sentence_query).lower()
+  #Put parameters in dictonary format so that for-loop below
+  #operates as expected.
+  sentences_dict = {name: sentence_query}
+#Just pull the control dict that has the search sentence for each audience
 else:
-    embedding_names = diet_query_embeddings_directories_list
-    embedding_sentence_query_lookup_df.write.mode("overwrite").format("delta").save(config.diet_query_embeddings_dir
-                                                                                    + dates.today +
-                                                                                    '/embedding_sentence_query_lookup')
-    for names in embedding_names:
-        names = names.replace(' ', '_')
-        names = names.replace('/', '')
-        saved_embedding_sentence_query = \
-            embedding_sentence_query_lookup_df.select(embedding_sentence_query_lookup_df.query
-                                                      ).where(embedding_sentence_query_lookup_df.name == names)
-        saved_embedding_sentence_query = saved_embedding_sentence_query.rdd.map(lambda column: column.query).collect()
-        saved_embedding_sentence_query = " ".join(saved_embedding_sentence_query)
-        # Encoding the query, make it a pyspark vector we can take the dot product of later
-        print(f'Encoding ({saved_embedding_sentence_query}) as the embedding sentence query for: {names}')
-        query_vector = utils.model.encode(saved_embedding_sentence_query, normalize_embeddings=True).tolist()
-        search_df = utils.create_search_df(utils.create_dot_df(product_vectors_df,
-                                                               utils.create_array_query(query_vector)))
-        search_df.write.mode("overwrite").format("delta").save(config.diet_query_embeddings_dir + dates.today + '/' +
-                                                               names)
+  sentences_dict = sentences_dict
 
+for name in sentences_dict.keys():
+  #Pull relevant sentence query
+  sentence_query = sentences_dict[name]
+  #Assert sentence query is a string
+  #Encoding the query and convert into list format to do the dot product
+  query_vector = utils.model.encode(sentence_query, normalize_embeddings=True).tolist()
+  #Do dot product between search query and vector from product embedding
+  search_df = utils.create_search_df(utils.create_dot_df(product_vectors_df,
+                                                          utils.create_array_query(query_vector)))
+  #Clean up audience name for more manageable format when writing out the directory
+  name = name.replace(' ', '_')
+  name = name.replace('/', '')
+  #Output search dataframe that outputted from the dot product for given audience
+  output_fp = config.diet_query_embeddings_dir + dates.today + '/' +name
+  search_df.\
+  write.\
+  mode("overwrite").\
+  format("delta").\
+  save(output_fp)
+
+  message = (
+    "Outputted {} for the given audience and query below \n".format(output_fp) +
+    "{}: {} \n \n".format(name, sentence_query)
+  )
+  print(message)
+
+#Code ran successfully, but warn the user that they
+#need to add inputted parameters to sentence_dict
+if not is_empty:
+  message = (
+    'UPC list was successfully created for {}! '.format(name) +
+    'Make sure to add the below key-value pair to the sentence_dict and git push it if you would like to it included in the automated workflow.' +
+    '\n' + '\n' +
+    '"{}": "{}"'.format(name, sentence_query)
+  )
+  raise Warning(message)
 
 # COMMAND ----------
 
